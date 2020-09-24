9ad79e644a3b5f98d1b4b53ed457c1fda9400dbd
Kurt Barry <kurt.m.barry@gmail.com>
2020-09-23 22:03:39 -0400

    initial impl

--- src/fib.sol
+++ src/fib.sol
@@ -1,4 +1,18 @@
 pragma solidity ^0.6.7;
 
-contract Fib {
+contract Fibber {
+
+    function fibonacciSum(uint256 nAdditions, uint256 f0, uint256 f1) internal pure returns (uint256 sum) {
+        for (uint256 i = 0; i < nAdditions; i++) {
+            sum = f0 + f1;
+            f0 = f1;
+            f1 = sum;
+        }
+    }
+
+    function fib(uint256 N, uint256 f0, uint256 f1) external pure returns (uint256) {
+        if (N == 0) return f0;
+        if (N == 1) return f1;
+        return fibonacciSum(N - 1, f0, f1);
+    }
 }
--- src/fib.t.sol
+++ src/fib.t.sol
@@ -2,20 +2,19 @@ pragma solidity ^0.6.7;
 
 import "ds-test/test.sol";
 
-import "./Fib.sol";
+import "./fib.sol";
 
 contract FibTest is DSTest {
-    Fib fib;
+    Fibber fibber;
 
     function setUp() public {
-        fib = new Fib();
+        fibber = new Fibber();
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
-
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function test_1_1() public {
+        uint256[10] memory firstTenValues = [uint256(1), 1, 2, 3, 5, 8, 13, 21, 34, 55];
+        for (uint256 i = 0; i < 10; i++) {
+            assertEq(fibber.fib(i, 1, 1), firstTenValues[i]);
+        }
     }
 }
