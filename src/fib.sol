ef2b6077ad7db828fefe2699331e825fb2e7cb21
Kurt Barry <kurt.m.barry@gmail.com>
2020-09-23 22:22:13 -0400

    just use a single function

--- src/fib.sol
+++ src/fib.sol
@@ -16,19 +16,16 @@
 pragma solidity ^0.6.7;
 
 contract Fibber {
-
-    function fibonacciSum(uint256 nAdditions, uint256 f0, uint256 f1) internal pure returns (uint256 sum) {
-        for (uint256 i = 0; i < nAdditions; i++) {
-            sum = f0 + f1;
-            require(sum >= f1);  // overflow
-            f0 = f1;
-            f1 = sum;
-        }
-    }
-
     function fib(uint256 N, uint256 f0, uint256 f1) external pure returns (uint256) {
         if (N == 0) return f0;
         if (N == 1) return f1;
-        return fibonacciSum(N - 1, f0, f1);
+        uint256 fN;
+        for (uint256 i = 0; i < N - 1; i++) {
+            fN = f0 + f1;
+            require(fN >= f1);  // overflow
+            f0 = f1;
+            f1 = fN;
+        }
+        return fN;
     }
 }
